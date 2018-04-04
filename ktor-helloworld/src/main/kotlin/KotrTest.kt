3b16694cf910f0addb490e47f1c819414258f9b2
fqc <fengqichao171106@credithc.com>
2018-04-04 11:40:46 +0800

    v0.1

--- ktor-helloworld/src/main/kotlin/KotrTest.kt
+++ ktor-helloworld/src/main/kotlin/KotrTest.kt
@@ -1,25 +1,22 @@
 import io.ktor.application.call
-import io.ktor.application.log
-import io.ktor.features.CallLogging
 import io.ktor.http.ContentType
-import io.ktor.request.ApplicationRequest
-import io.ktor.request.uri
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.application.log.info("访问来了: " + call.request.uri)
+                //                call.application.log.info("访问来了: " + call.request.uri)
                 call.respondText("hello ktor", ContentType.Text.Plain)
             }
 
             get("/demo") {
-                call.application.log.info("访问来了: " + call.request.uri)
+                //                call.application.log.info("访问来了: " + call.request.uri)
                 call.respondText("HELLO WORLD")
             }
         }
